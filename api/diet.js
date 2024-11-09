9d1505e98a5ea9c45a83f48b6173987fd18eaf70
didam00 <didam00@naver.com>
2024-11-09 13:27:48 +0900

    refactor: 'require' change to 'import'

--- api/diet.js
+++ api/diet.js
@@ -1,5 +1,5 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 const [BREAKFAST, LUNCH, DINNER] = [0, 1, 2];
 const DAY_TIMES = 24 * 60 * 60 * 1000;
