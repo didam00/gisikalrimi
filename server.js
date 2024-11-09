7adec129e5b7352a57b41d91335bca70ecc3eb8a
didam00 <didam00@naver.com>
2024-11-09 13:16:51 +0900

    refactor: remove index.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// const http = require('http');
+// const handler = require('./api/diet').default;
+
+// // 로컬 서버 포트 설정
+// const PORT = 5001;
+
+// const server = http.createServer((req, res) => {
+//   // 요청 및 응답 객체를 핸들러에 전달
+//   handler(req, res);
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
